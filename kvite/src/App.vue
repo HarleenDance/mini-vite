3786fb4f8874b5d85a4fc523ddd4b38fab36f824
Harleens <66819607+a239529505@users.noreply.github.com>
2022-04-09 23:00:33 +0800

    Add files via upload

---
+++ kvite/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        {{title}}
+    </div>
+</template>
+
+<script>
+    import {
+        reactive,
+        toRefs,
+        onBeforeMount,
+        onMounted
+    } from 'vue'
+    export default {
+        name: '',
+        setup() {
+            const state = reactive({
+                title: 'hello kvite!!!!'
+            })
+            onBeforeMount(() => {})
+            onMounted(() => {})
+            return {
+                ...toRefs(state),
+            }
+
+        }
+    };
+</script>
+<style lang='scss' scoped>
+</style>
